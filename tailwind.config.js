3468d3873f1abdc6321fa7b2eb93e7c2f59910bc
Ropats <rohitcpathare@gmail.com>
2023-04-18 12:33:17 +0530

    css setup with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from 'daisyui'
+import typography from '@tailwindcss/typography'
+
+export default {
+  content: ['./index.html', './src/**/*.{svelte,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins']
+      }
+    },
+  },
+  plugins: [typography, daisyui],
+}
