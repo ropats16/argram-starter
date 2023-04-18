d7c1f6b673fbb35b51813771f66b92a478b7f18d
Ropats <rohitcpathare@gmail.com>
2023-04-18 12:41:10 +0530

    Add supporting popup dialogs

---
+++ src/dialogs/confirm.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Modal from "../components/modal.svelte";
+  import { createEventDispatcher } from "svelte";
+  export let open = false;
+  export let tx = "";
+  const dispatch = createEventDispatcher();
+</script>
+
+<Modal {open} on:cancel={() => dispatch("cancel")}>
+  <div
+    class="m-4 border-4 border-success pb-8 px-2 flex flex-col items-center justify-center"
+  >
+    <h3 class="mb-8 text-3xl bg-success text-white p-2 mt-4">Success!</h3>
+    <p class="text-sm">{tx}</p>
+    <!--
+    <blockquote>
+      NOTE: When uploading large images it may take a few minutes for the image
+      to show up on the network.
+    </blockquote>
+    -->
+    <a href="https://arweave.net/tx/{tx}" class="link" target="_blank"
+      >View Atomic TX</a
+    >
+  </div>
+</Modal>
