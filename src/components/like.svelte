99eebf2c51d71a7c82608ebed200295632563900
Ropats <rohitcpathare@gmail.com>
2023-04-20 18:09:04 +0530

    Add likes component

---
+++ src/components/like.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {
+    readContractWOthent,
+    writeContractWOthent,
+  } from "permawebjs/contract";
+  import { profile } from "../store";
+  import { onMount } from "svelte";
+
+  export let id = "";
+
+  let likes = {};
+
+  async function likePost() {
+    // Code for liking posts
+  }
+
+  async function readLikes() {
+    // Code for reading likes
+  }
+
+  onMount(async () => {
+    // Code for fetching and rendering likes on app refresh
+  });
+</script>
+
+<section>
+  <p>{Object.keys(likes).length}</p>
+  <button
+    class="btn btn-block"
+    disabled={Object.keys(likes).includes($profile.contract_id)}
+    on:click|preventDefault={likePost}>Like</button
+  >
+</section>
