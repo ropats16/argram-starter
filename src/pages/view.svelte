34f0fc63fbbe5ee21efe577566b9559957ebf6a9
Ropats <rohitcpathare@gmail.com>
2023-04-20 18:12:33 +0530

    view page update

--- src/pages/view.svelte
+++ src/pages/view.svelte
@@ -1,14 +1,8 @@
 <script>
-  import { onMount } from "svelte";
-  import { getAssetData } from "../lib/asset";
+  import { getAssetData } from "../lib/queryAssets";
   import Comment from "../components/comment.svelte";
-
-  let assetData = getAssetData();
-  let assets = [];
-
-  onMount(async () => {
-    assets = await assetData;
-  });
+  import { take } from "ramda";
+  import Like from "../components/like.svelte";
 </script>
 
 <section class="hero min-h-screen bg-base-100 flex flex-col">
@@ -18,36 +12,45 @@
   </p>
   <hr class="w-1/2 mt-2" />
   <!-- Get relevant data for assets from getAssetData function -->
-  {#await assetData then assets}
+  {#await getAssetData() then assets}
     <!-- If assets array is non empty, map over assets to render data -->
     {#if assets.length > 0}
       <div class="flex-col">
         {#each assets as asset}
-          <div class="hero-content py-5 m-0 flex-col md:space-x-4">
-            <div class="w-1/2 px-0 mx-0 grid place-items-center">
+          <div
+            class="hero-content my-5 flex-col md:space-x-4 border-solid border-2 border-slate-300 rounded-lg"
+          >
+            <div class="w-7/8 px-0 mx-0 grid place-items-center">
               {#if asset.type === "image"}
                 <img
-                  class="w-full md:w-[500px] object-contain"
+                  class="w-[350px] object-contain rounded-lg"
                   src={asset.image}
                   alt={asset.title}
                 />
               {/if}
             </div>
-            <div class="w-[325px] md:w-1/2 md:ml-8">
-              <div class="mb-4 flex items-start justify-between">
-                <strong class="text-xl">{asset.title}</strong>
+            <div class="flex flex-row items-center justify-center">
+              <div class="w-7/8 mx-0">
+                <div class="w-[320px] flex justify-between">
+                  <p class="text-sm">
+                    {take(5, asset.owner)}
+                    <strong class="text-md">{asset.title}</strong>
+                  </p>
+                </div>
+                <p class="text-sm text-gray-600">{asset.description}</p>
+                {#if asset.topics.length > 0}
+                  <p class="text-sm text-gray-400">
+                    Hashtags: {asset.topics.join(", ")}
+                  </p>
+                {/if}
+                <p class="text-xs text-gray-300">
+                  {new Date(asset.timestamp).toDateString()}
+                </p>
               </div>
-              <p class="text-xl">{asset.description}</p>
-              {#if asset.topics.length > 0}
-                <p class="mt-4 text-sm">Topics: {asset.topics.join(", ")}</p>
-              {/if}
-              <p class="text-xs">
-                {new Date(asset.timestamp).toDateString()}
-              </p>
+              <Like id={asset.id} />
             </div>
+            <Comment id={asset.id} />
           </div>
-          <Comment id={asset.id} />
-          <hr />
         {/each}
       </div>
     {:else}
